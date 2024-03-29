6214690ce6e593a2b308c7caafe9502c8985c4ca
JGS <1847014382@qq.com>
2024-03-29 18:04:59 +0800

    1.新增滑动窗口例题 苦难例题

---
+++ TestJMH-master/src/main/java/com/mashibing/jmh/leetCodeDoublePointer/test33_minWindow.java
@@ -0,0 +1,19 @@
+package com.mashibing.jmh.leetCodeDoublePointer;
+
+/**
+ * @Auther：jinguangshuai
+ * @Data：2024/3/29 - 03 - 29 - 17:22
+ * @Description:com.mashibing.jmh.leetCodeDoublePointer
+ * @version:1.0
+ */
+public class test33_minWindow {
+
+    // s 字符串  t 待匹配字符串
+    public static String minWindow(String s, String t) {
+        if(s.length() < t.length()){
+            return "";
+        }
+
+        return "";
+    }
+}
