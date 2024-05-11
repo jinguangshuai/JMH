c33442c7d30af30b6f808ef0e6299e0331b6d098
JGS <1847014382@qq.com>
2024-05-11 17:50:44 +0800

    1.新增字典树（前缀树） 单词搜索

--- TestJMH-master/src/main/java/com/jgs/jmh/leetCode13_DictionaryTree/test100_findWords.java
+++ TestJMH-master/src/main/java/com/jgs/jmh/leetCode13_DictionaryTree/test100_findWords.java
@@ -7,6 +7,8 @@ package com.jgs.jmh.leetCode13_DictionaryTree;
  * @version:1.0
  */
 
+import java.util.List;
+
 /**
  * * 给定一个 m x n 二维字符网格 board 和一个单词（字符串）列表 words， 返回所有二维网格上的单词 。
  *
@@ -14,4 +16,21 @@ package com.jgs.jmh.leetCode13_DictionaryTree;
  * * 同一个单元格内的字母在一个单词中不允许被重复使用。
  */
 public class test100_findWords {
+
+    public List<String> findWords(char[][] board, String[] words) {
+        //构建字典树（递归）
+        //根据单词前缀获取是否可构成单词
+        //最后返回结果
+        return null;
+    }
+
+    public class Trie{
+        Trie[] children;
+        public Trie(){
+            children = new Trie[26];
+        }
+        public void insert(String word) {
+            
+        }
+    }
 }
